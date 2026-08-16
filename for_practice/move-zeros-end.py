e0526573256738248103935a3acad817f27e8d1e
Hell0Ankit <ankitbohra660@gmail.com>
2026-08-16 11:35:19 +0530

    practice: rename pointers name

--- for_practice/move-zeros-end.py
+++ for_practice/move-zeros-end.py
@@ -3,13 +3,13 @@
 # Output: [1, 3, 12, 0, 0]
 class Solution(object):
     def move_zeros_end(self, arr_num):
-        left = 0
-        right = 0
-        while right < len(arr_num):
-            if arr_num[right]!=0:
-                arr_num[left],arr_num[right] = arr_num[right],arr_num[left]
-                left += 1
-            right += 1
+        i = 0
+        j = 0
+        while j < len(arr_num):
+            if arr_num[j]!=0:
+                arr_num[i],arr_num[j] = arr_num[j],arr_num[i]
+                i += 1
+            j += 1
         return arr_num
         
 arr_num =  [0, 1, 0, 3, 12]
